1572099eaf7568dac71d52949eec2ff8c7160258
soenneker <jake@soenneker.com>
2026-08-29 14:17:24 -0500

    Improve public API XML documentation

--- src/Soenneker.Tests.Logging/Abstract/ILoggingTest.cs
+++ src/Soenneker.Tests.Logging/Abstract/ILoggingTest.cs
@@ -18,5 +18,10 @@ public interface ILoggingTest
     /// <summary>
     /// Wraps Task.Delay with a log statement. Should be used for delays in tests. <para/>
     /// </summary>
+    /// <param name="millisecondsDelay">Milliseconds Delay for the delay operation.</param>
+    /// <param name="reason">Reason for the delay operation.</param>
+    /// <param name="log">Whether log.</param>
+    /// <param name="cancellationToken">Token used to cancel the operation.</param>
+    /// <returns>A task that completes when the delay operation is complete.</returns>
     Task Delay(int millisecondsDelay, string? reason = null, bool log = true, CancellationToken cancellationToken = default);
 }
